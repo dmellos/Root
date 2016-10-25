09993d705be02dbda8d10419cb49161626e00983
SELWYN DMELLO <dmelloselwyn@gmail.com>
2016-10-24 23:25:54 -0400

    Update SurveyException.java

--- src/com/company/surveymanagement/Exception/SurveyException.java
+++ src/com/company/surveymanagement/Exception/SurveyException.java
@@ -3,7 +3,7 @@ package com.company.surveymanagement.Exception;
 public class SurveyException extends Exception{
 	
 	public SurveyException() {
-		// TODO Auto-generated constructor stub
+		
 	}
 	public SurveyException(String Message) {
 		
